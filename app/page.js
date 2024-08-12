bf8497dbed06461bd6428fec3f1f4410fd20bd7b
mmalaika.junaid <mmalaika.junaid@gmail.com>
2024-08-12 23:05:53 +0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,9 +8,6 @@ export default function Home() {
   return (
     <main className="flex flex-col h-screen bg-[#040d17] text-white">
       <nav className="flex justify-between items-center p-4">
-        <h1 className="text-xl font-semibold">
-          <span className="highlighted-text">MAAN</span>
-        </h1>
       </nav>
       <div className="flex-grow overflow-hidden">
         <ChatApp />
